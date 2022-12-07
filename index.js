8b00b3602884ba82132bcdb2751db332f1d82af7
FlaviaCaroliny <114813467+FlaviaCaroliny@users.noreply.github.com>
2022-12-06 21:12:53 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+var n=prompt('Digite seu nome aqui:')
+var an=Number(prompt('Digite seu ano de nascimento'))
+let at=2022
+let i=at-an;
+let m1=('Olá, seu nome é ' +n);
+let m2=('E sua idade é ' +i);
+alert(m1);
+alert(m2);
